130d61d9f4812cc51478aa23da22415c14c16109
Mia-Y-Kelly <miayn24@gmail.com>
2022-04-25 02:06:08 -0400

    remove duplicate authenticate

--- db.php
+++ db.php
@@ -334,26 +334,7 @@
             die();
         }
     }
-        $sqlstmt = "SELECT count(*) FROM
-                    (SELECT stu_acc AS username, stu_pwd AS password FROM Student
-                    UNION
-                    SELECT instr_acc AS username, instr_pwd AS password FROM Instructor) combined ";
-
-        $statement = $dbh->prepare($sqlstmt.
-                                    " where username = :username and password = sha2(:passwd,256) ");
-        $statement->bindParam(":username", $user);
-        $statement->bindParam(":passwd", $passwd);
-        $result = $statement->execute();
-        $row=$statement->fetch();
-        $dbh=null;
-	//	print $row[0];
-		
-        return $row[0];
-    }catch (PDOException $e) {
-        print "Error! " . $e->getMessage() . "<br/>";
-        die();
-    }
-}
+    
 
 // Currently working on detecting if password reset should occur
 function isFirstLogin() {
